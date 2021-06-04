c1441b4a94f83203a73a3fdaf5ebc9df63c74968
Lawrence Gumbo <84715458+lawrence-sert@users.noreply.github.com>
2021-06-04 07:18:45 +0200

    Update recipe-dish.component.ts

--- src/app/recipe/recipe-dish/recipe-dish.component.ts
+++ src/app/recipe/recipe-dish/recipe-dish.component.ts
@@ -2,46 +2,47 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import {Recipes} from 'src/app/models/recipes.model';
-import { RecipesService} from '../../services/recipes.service'
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipe-dish',
   templateUrl: './recipe-dish.component.html',
-  styleUrls: ['./recipe-dish.component.css'],
+  styleUrls: ['./recipe-dish.component.css']
 })
 export class RecipeDishComponent implements OnInit {
+
   recipeRef: AngularFirestoreCollection<Recipes>;
   recipe$: Observable<Recipes[]>;
   recipeID: any[] = [];
-  recipes: any;
-  id: Observable<any> | undefined;
   public parameterValue: any[] = [];
 
-  constructor(private recipeServices: RecipesService, private router: Router,private activatedRoute: ActivatedRoute,public db: AngularFirestore)
-   {
-    this.recipeRef = this.db.collection('recipes');
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    public db: AngularFirestore
+    ) { 
+
+    this.activatedRoute.params.subscribe(parameter => {
+      this.parameterValue = parameter.recipeID
+      console.log(this.parameterValue);
+    });
+
+    this.recipeRef = this.db.collection<{}>('recipes', ref => ref.where('recipeID', '==', this.parameterValue));
     this.recipe$ = this.recipeRef.snapshotChanges().pipe(
-      map((actions) => {
-        return actions.map((action) => {
-          const data = action.payload.doc.data() as Recipes;
-          return {
-            id: action.payload.doc.id,
-            name: data.name,
-            description: data.description,
-          };
-        });
-      })
-    );
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data(); // DB Questions
+        const id = a.payload.doc.id;
+        return { id, ...data };
+
+        console.log(this.recipe$);
+      }))
+      );
   }
 
   ngOnInit(): void {
-   this.id =  this.activatedRoute.snapshot.params['id'];
-   this.recipeServices.getRecipeDetails(this.id).subscribe((recipe: any) => {
-      console.log('recipe details:' +JSON.stringify(recipe));
-    });
+
+
   }
+
 }
-
-
